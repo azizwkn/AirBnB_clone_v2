091ba75bf83bc9a10d3b998b0dcf5a8434bc0479
Aziz <ngwk.zz@outlook.com>
2023-12-26 11:19:38 +0300

    Display a number

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -38,8 +38,7 @@ def display_python_is(text="is cool"):
 @app.route("/number/<n>", strict_slashes=False)
 def display_number(n):
     """Displays 'n is a number' only if n is an integer"""
-
-    return f"{int(n)} is a number"
+    return "{:d} is a number".format(n)
 
 
 if __name__ == "__main__":
